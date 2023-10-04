4f78f5a7e6f16b9237af946965647ac20f7996da
Зарубєй Олександр Ігорович <aleksandr.zarubey@fuib.com>
2023-10-04 17:39:38 +0300

    Lesson Three part one commit

---
+++ src/Cat.java
@@ -0,0 +1,23 @@
+import javax.lang.model.element.Name;
+
+public class Cat extends Animal {
+    private static int totalCats = 0;
+    public Cat(String name) {
+        super(name, 200, 0);
+        totalCats++;
+    }
+
+    public static int getTotalCats() {
+        return totalCats;
+    }
+    @Override
+    public void swim(int distance) {
+        if (distance <= 0) {
+            System.out.println(name + " не може пропливти від'ємну відстань і взагалі коти не плавають");
+        } else {
+            System.out.println("Кіт " + name +" не вміє плавати");
+        }
+
+    }
+
+}
