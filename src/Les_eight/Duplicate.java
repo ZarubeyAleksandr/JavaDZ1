363d6f3f3744be4a0bd0cdc7970a9c8d7c7f3c46
Зарубєй Олександр Ігорович <aleksandr.zarubey@fuib.com>
2023-11-26 18:06:50 +0200

    DZ 8. Tasks

---
+++ src/Les_eight/Duplicate.java
@@ -0,0 +1,28 @@
+package Les_eight;
+
+import java.util.HashMap;
+
+public class Duplicate {
+
+    public static boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> myMap = new HashMap<>();
+        for (int num : nums) {
+            if (myMap.containsKey(num))
+                return true;
+            myMap.put(num, 1);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 1};
+        System.out.println(containsDuplicate(nums1));
+
+        int[] nums2 = {1, 2, 3, 4};
+        System.out.println(containsDuplicate(nums2));
+
+        // Example 3
+        int[] nums3 = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+        System.out.println(containsDuplicate(nums3));
+    }
+}
---
+++ src/Les_eight/Palindrome.java
@@ -0,0 +1,47 @@
+package Les_eight;
+
+import static java.lang.Character.isLetterOrDigit;
+import static java.lang.Character.toLowerCase;
+
+public class Palindrome {
+
+    public static boolean isPalindrome(String s) {
+        if (s.isEmpty()) {
+            return true;
+        }
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            char leftChar = s.charAt(left);
+            char rightChar = s.charAt(right);
+
+            if (!isLetterOrDigit(leftChar)) {
+                left++;
+            } else if (!isLetterOrDigit(rightChar)) {
+                right--;
+            } else {
+                if (toLowerCase(leftChar) !=
+                        toLowerCase(rightChar)) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String inputString1 = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(inputString1));
+
+        String inputString2 = "raceacar";
+        System.out.println(isPalindrome(inputString2));
+
+        String inputString3 = "   ";
+        System.out.println(isPalindrome(inputString3));
+
+    }
+}
